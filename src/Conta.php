c86e3f912b08191652944a11fde5767816ba6d44
heliobrandao <heliob79@gmail.com>
2022-02-21 11:19:10 -0300

    first commit

---
+++ src/Conta.php
@@ -0,0 +1,75 @@
+<?php
+
+class Conta
+{
+    
+    private string $cpfTitular;
+    private string $nomeTitular;
+    private float $saldo = 0;
+
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+
+    }
+
+    public function saca(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+//após a criação do construtor os métodos defineNomeTitular e defineCpfTitular não são mais necessários
+    // public function defineCpfTitular(string $cpf): void //setCpfTitular
+    // {
+    //     $this->cpfTitular = $cpf;
+    // }
+
+    // public function defineNomeTitular(string $nome): void
+    // {
+    //     $this->nomeTitular = $nome;
+    // }
+
+    public function recuperaSaldo(): float //getSaldo
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+    
+
+}
